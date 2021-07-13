3ef16cd5d59e8bc542627036cc688582482b9042
Jem Alvarez <jemalvarez22@gmail.com>
2021-07-13 16:06:22 -0400

    Finish news WebView

--- COVID Tracker/View/NewsView.swift
+++ COVID Tracker/View/NewsView.swift
@@ -6,43 +6,55 @@ struct NewsView: View {
     @ObservedObject var model = NewsViewModel()
     
     var body: some View {
-        ZStack {
-            Color(Assets.shared.getColor(.snow)).edgesIgnoringSafeArea(.all)
-            
-            ScrollView {
-                HStack {
-                    Text("News Articles \(model.news != nil ? (model.news!.totalResults > 50 ? "(50)" : "(\(model.news!.totalResults))") : "")")
-                        .font(.title.bold())
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        model.sort.toggle()
-                        if model.news != nil {
-                            model.sort(array: model.news!.articles, descending: model.sort)
-                        }
-                    }) {
-                        Image(systemName: model.sort ? "arrow.down.doc.fill" : "arrow.up.doc.fill")
-                            .font(.title2)
-                    }
-                }
-                .padding()
-                .padding(.top)
-                .foregroundColor(Color(Assets.shared.getColor(.leafy)))
+        NavigationView {
+            ZStack {
+                Color(Assets.shared.getColor(.snow)).edgesIgnoringSafeArea(.all)
                 
-                LazyVStack {
-                    if model.news != nil {
-                        ForEach(0..<model.news!.articles.count) { i in
-                            NewsRowView(title: model.news!.articles[i].title, description: model.news!.articles[i].description, source: model.news!.articles[i].source.name, date: model.news!.articles[i].publishedAt.isoToDate().format("MMMM d, yyyy h:mm a"))
+                ScrollView {
+                    HStack {
+                        Text("News Articles \(model.news != nil ? (model.news!.totalResults > 50 ? "(50)" : "(\(model.news!.totalResults))") : "")")
+                            .font(.title.bold())
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            model.sort.toggle()
+                            if model.news != nil {
+                                model.sort(array: model.news!.articles, descending: model.sort)
+                            }
+                        }) {
+                            Image(systemName: model.sort ? "arrow.down.doc.fill" : "arrow.up.doc.fill")
+                                .font(.title2)
                         }
-                    } else {
-                        ProgressView()
-                            .colorScheme(.light)
+                    }
+                    .padding()
+                    .padding(.top)
+                    .foregroundColor(Color(Assets.shared.getColor(.leafy)))
+                    
+                    LazyVStack {
+                        if model.news != nil {
+                            ForEach(0..<model.news!.articles.count) { i in
+                                NavigationLink(destination:
+                                                WebView(url: URL(string: model.news!.articles[i].url)!)
+                                                .navigationTitle(model.news!.articles[i].title)
+                                                .navigationBarTitleDisplayMode(.inline)
+                                )
+                                {
+                                    NewsRowView(title: model.news!.articles[i].title, description: model.news!.articles[i].description, source: model.news!.articles[i].source.name, date: model.news!.articles[i].publishedAt.isoToDate().format("MMMM d, yyyy h:mm a"))
+                                }
+                            }
+                        } else {
+                            ProgressView()
+                                .colorScheme(.light)
+                        }
                     }
                 }
+                .padding(.bottom)
             }
-            .padding(.bottom)
+            .navigationTitle("")
+            .navigationBarHidden(true)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
